84e431dccc5890bcf898242b7106d90783bc8344
pablov <pevweb@gmail.com>
2020-01-20 13:48:42 -0400

    .

---
+++ print.py
@@ -0,0 +1,19 @@
+from escpos.connections import getUSBPrinter
+
+
+#printer = getUSBPrinter()(idVendor=0x04F9,
+ #                         idProduct=0x20A8,
+  #                        inputEndPoint=0x82,
+   #                       outputEndPoint=0x01)
+#
+#This
+#47
+#Decimal VendorID=2867 & ProductID=1794
+#39
+#Hexadecimal VendorID=0xb33 & ProductID=0x702
+
+
+printer = getUSBPrinter(commandSet='Generic')(idVendor=0x04f9, idProduct=0x20a8)
+#dev = usb.core.find(idVendor=0x04F9, idProduct=0x20A8)
+printer.text("Hello World")
+printer.lf()
